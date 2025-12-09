f00a25ceb3343364d80cf4deb3ec0f9aacf2f33b
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-12-09 20:11:06 +0200

    Colors

---
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/theme/Colors.kt
@@ -0,0 +1,18 @@
+package org.tues.tudy.ui.theme
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor1 = Color(0xFFFF842C)
+val PrimaryColor2 = Color(0xFFFFC02E)
+
+val SecondaryColor1 = Color(0xFFF2F2F2)
+
+val BaseColor0 = Color(0xFFFFFCFA)
+val BaseColor20 = Color(0xFFE6E6E6)
+val BaseColor40 = Color(0xFFD1D1D1)
+val BaseColor60 = Color(0xFFABABAB)
+val BaseColor80 = Color(0xFF7D7D7D)
+val BaseColor100 = Color(0xFF221F1F)
+
+val SuccessColor = Color(0xFF10E260)
+val WarningColor = Color(0xFFFFA323)
+val ErrorColor = Color(0xFFE92405)
