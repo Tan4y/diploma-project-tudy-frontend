f813e625806064714491edf19230fd8e3a781eca
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-12-10 23:34:31 +0200

    AuthRepository throw exceptions except string and add network security

--- composeApp/src/androidMain/kotlin/org/tues/tudy/data/remote/AuthRepository.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/data/remote/AuthRepository.kt
@@ -2,6 +2,7 @@ package org.tues.tudy.data.remote
 
 import org.tues.tudy.data.model.CreateAccountRequest
 import org.tues.tudy.data.model.LogInRequest
+import retrofit2.HttpException
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,25 +14,19 @@ class AuthRepository {
         .build()
         .create(ApiService::class.java)
 
-    suspend fun createAccount(username: String, email: String, password: String): String {
+    suspend fun createAccount(username: String, email: String, password: String) {
         val response = api.register(CreateAccountRequest(username, email, password))
 
-        if (response.isSuccessful) {
-            return "Check your email to verify your account"
-        } else {
-            val error = response.errorBody()?.string()
-            return error ?: "Unknown error"
+        if (!response.isSuccessful) {
+            throw HttpException(response)
         }
     }
 
-    suspend fun login(username: String, password: String): String {
+    suspend fun login(username: String, password: String) {
         val response = api.login(LogInRequest(username, password))
 
-        if (response.isSuccessful) {
-            return "Successful log in"
-        } else {
-            val error = response.errorBody()?.string()
-            return error ?: "Unknown error"
+        if (!response.isSuccessful) {
+            throw HttpException(response)
         }
     }
 }
