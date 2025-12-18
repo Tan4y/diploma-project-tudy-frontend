0f59510c9475b5e9191502877d7601c7b01e6de4
Tan4y <100035220+Tan4y@users.noreply.github.com>
2025-12-18 03:00:28 +0200

    Remove unuseful code and modify it in MainActivity

--- composeApp/src/androidMain/kotlin/org/tues/tudy/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/MainActivity.kt
@@ -1,33 +1,30 @@
 package org.tues.tudy
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import org.tues.tudy.ui.common.SuccessErrorScreen
 import org.tues.tudy.ui.navigation.AppNavGraph
+import org.tues.tudy.ui.navigation.Routes
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
-            val navController = rememberNavController()
-            Surface(color = MaterialTheme.colorScheme.background) {
-                AppNavGraph(navController)
-            }
+            navController = rememberNavController()
+            AppNavGraph(navController)
         }
     }
-}
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+    }
 }
