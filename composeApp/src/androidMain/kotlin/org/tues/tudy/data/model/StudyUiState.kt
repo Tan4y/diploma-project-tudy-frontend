c643fe92f4ee5234d974f516e662ffa9338fb11c
Tan4y <100035220+Tan4y@users.noreply.github.com>
2026-01-16 02:35:55 +0200

    Model and svg flash

---
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/data/model/StudyUiState.kt
@@ -0,0 +1,11 @@
+package org.tues.tudy.data.model
+
+import org.tues.tudy.ui.components.StudyPhase
+
+data class StudyUiState(
+    val phase: StudyPhase = StudyPhase.IDLE,
+    val remainingSeconds: Int = 0,
+    val totalSeconds: Int = 0,
+    val currentRound: Int = 0,
+    val maxRounds: Int = 4
+)
