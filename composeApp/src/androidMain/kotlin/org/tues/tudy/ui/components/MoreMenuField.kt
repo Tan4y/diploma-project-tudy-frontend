3d5e142e8652b43e86c15cf0ac4f92433b1b3893
Tan4y <100035220+Tan4y@users.noreply.github.com>
2026-01-20 03:10:10 +0200

    New components for more menu

---
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/components/MoreMenuField.kt
@@ -0,0 +1,41 @@
+package org.tues.tudy.ui.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.tues.tudy.ui.theme.AppTypography
+import org.tues.tudy.ui.theme.BaseColor100
+import org.tues.tudy.ui.theme.Dimens
+
+@Composable
+fun MoreMenuField(
+    text: String,
+    onClick: () -> Unit,
+    color: Color
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = Dimens.Space125)
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }) { onClick() },
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = text,
+            style = AppTypography.Heading5,
+            color = color
+        )
+    }
+}
