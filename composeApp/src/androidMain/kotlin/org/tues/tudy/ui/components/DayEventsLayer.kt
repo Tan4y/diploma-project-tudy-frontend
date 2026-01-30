8d514003b4d5a1e6d589163299a4d157e204530c
Tan4y <100035220+Tan4y@users.noreply.github.com>
2026-01-30 03:38:06 +0200

    Add fields and cases in DayEventCard

--- composeApp/src/androidMain/kotlin/org/tues/tudy/ui/components/DayEventsLayer.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/components/DayEventsLayer.kt
@@ -2,19 +2,36 @@ package org.tues.tudy.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.navigation.NavController
 import org.tues.tudy.data.model.CalendarItem
+import org.tues.tudy.ui.theme.Dimens
 import kotlin.collections.forEach
 
 @Composable
-fun DayEventsLayer(events: List<CalendarItem>, startHour: Int,
-                   endHour: Int) {
-    Box(
-        modifier = Modifier.fillMaxHeight()
-    ) {
+fun DayEventsLayer(
+    events: List<CalendarItem>,
+    startHour: Int,
+    endHour: Int,
+    navController: NavController,
+    userId: String,
+    minuteHeightDp: Dp,
+    onDeleteEvent: (CalendarItem) -> Unit
+) {
+    Box(modifier = Modifier.fillMaxHeight().padding(vertical = Dimens.Space50)) {
         events.forEach { item ->
-            DayEventCard(item, startHour, endHour)
+            DayEventCard(
+                item = item,
+                startHour = startHour,
+                endHour = endHour,
+                navController = navController,
+                userId = userId,
+                minuteHeightDp = minuteHeightDp,
+                onDelete = onDeleteEvent
+            )
         }
     }
 }
