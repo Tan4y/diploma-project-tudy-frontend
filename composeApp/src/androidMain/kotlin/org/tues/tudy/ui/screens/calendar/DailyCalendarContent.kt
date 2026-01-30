375797926cbf65bcac42f33055821230d3312be0
Tan4y <100035220+Tan4y@users.noreply.github.com>
2026-01-30 03:40:39 +0200

    Add types and buttons in DailyCalendarContent

--- composeApp/src/androidMain/kotlin/org/tues/tudy/ui/screens/calendar/DailyCalendarContent.kt
+++ composeApp/src/androidMain/kotlin/org/tues/tudy/ui/screens/calendar/DailyCalendarContent.kt
@@ -6,21 +6,31 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import org.tues.tudy.data.model.CalendarItem
+import org.tues.tudy.ui.components.BasePopUp
 import org.tues.tudy.ui.components.DayEventsLayer
 import org.tues.tudy.ui.components.HourGrid
 import org.tues.tudy.ui.components.TimeColumn
 import org.tues.tudy.ui.theme.Dimens
 import org.tues.tudy.viewmodel.CalendarViewModel
+import kotlin.math.min
 
 @Composable
 fun DailyCalendarContent(
     viewModel: CalendarViewModel,
     modifier: Modifier = Modifier,
-    innerPadding: PaddingValues = PaddingValues(0.dp)
+    innerPadding: PaddingValues = PaddingValues(0.dp),
+    navController: NavController,
+    userId: String
 ) {
     val selectedDate = viewModel.selectedDay.collectAsState().value
     val day = viewModel.days.collectAsState().value
@@ -33,11 +43,47 @@ fun DailyCalendarContent(
     val totalHours = endHour - startHour
 
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-    val paddingTopBottomBars = innerPadding.calculateTopPadding() + innerPadding.calculateBottomPadding()
+    val paddingTopBottomBars =
+        innerPadding.calculateTopPadding() + innerPadding.calculateBottomPadding()
 
     val paddingExtra = Dimens.Space150 * 2
     val availableHeight = screenHeight - paddingTopBottomBars - paddingExtra
     val hourHeightDp: Dp = availableHeight / totalHours
+
+    val baseMinuteHeight = hourHeightDp / 60f
+
+    val minEventHeight = 80.dp
+
+    val scaleFactor = min(
+        5f,
+        maxOf(1f, minEventHeight.value / (15 * baseMinuteHeight.value))
+    )
+
+    val minuteHeightDp = baseMinuteHeight * scaleFactor
+    val scaledHourHeightDp = minuteHeightDp * 60
+
+    var showConfirm by remember { mutableStateOf(false) }
+    var eventToDelete by remember { mutableStateOf<CalendarItem?>(null) }
+    if (showConfirm && eventToDelete != null) {
+        BasePopUp(
+            onDismiss = {
+                showConfirm = false
+                eventToDelete = null
+            },
+            onConfirm = {
+                viewModel.deleteEvent(
+                    eventId = eventToDelete!!.id,
+                    userId = userId
+                )
+                showConfirm = false
+                eventToDelete = null
+            },
+            title = "Delete event",
+            description = "Are you sure you want to delete this event?",
+            buttonText = "Delete"
+        )
+    }
+
 
     Column(
         modifier = modifier
@@ -58,7 +104,7 @@ fun DailyCalendarContent(
                 TimeColumn(
                     startHour = startHour,
                     endHour = endHour,
-                    hourHeightDp = hourHeightDp
+                    hourHeightDp = scaledHourHeightDp
                 )
 
                 // Events column
@@ -69,12 +115,21 @@ fun DailyCalendarContent(
                     HourGrid(
                         startHour = startHour,
                         endHour = endHour,
-                        hourHeightDp = hourHeightDp
+                        hourHeightDp = scaledHourHeightDp
                     )
                     DayEventsLayer(
-                        day.items, startHour = startHour,
-                        endHour = endHour
+                        events = day.items,
+                        startHour = startHour,
+                        endHour = endHour,
+                        navController = navController,
+                        userId = userId,
+                        minuteHeightDp = minuteHeightDp,
+                        onDeleteEvent = { event ->
+                            eventToDelete = event
+                            showConfirm = true
+                        }
                     )
+
                 }
             }
         }
